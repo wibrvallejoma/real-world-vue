0062f9a7e66cf5f62dc2286cafca1a75750c9c8f
Williams <wibrvallejoma@gmail.com>
2019-04-21 11:46:11 -0600

    Added files

---
+++ src/components/EventCard.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about-us',
+      name: 'about',
+      component: About,
+      alias: '/about'
+    }
+    /*
+    {
+      path: '/about',
+      redirect: { name: 'about' }
+    } */
+  ]
+})
